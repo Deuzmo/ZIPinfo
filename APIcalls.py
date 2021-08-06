3435ceb8f3efcc3e77d6accb2e525ddbee7e81f6
Deuzmo <dancardp@gmail.com>
2021-08-05 23:24:24 -0700

    Finished implementation of app

--- APIcalls.py
+++ APIcalls.py
@@ -4,4 +4,9 @@ import requests
 
 def findby_country_and_zip(country, zip):
     response = requests.get("https://api.zippopotam.us/{}/{}".format(country,zip))
-    return response.json()
+    if (response.status_code >= 400):
+        return {"error" : "Error! tried to access wrong ZIP code"}
+    elif (response.status_code >= 200 and response.status_code < 300):
+        return response.json()
+    else:
+        return {"error": "Error! Something is not working with the server"}
--- App.py
+++ App.py
@@ -1,11 +1,16 @@
 import tkinter as tk
+from tkinter.constants import RIDGE
+import tkinter.ttk as ttk
+from countrylist import supported_countries, get_country_code
+from APIcalls import findby_country_and_zip
 
 
 class App(tk.Frame):
-    def __init__(self, root, *args, **kwargs):
+    def __init__(self, root, countries, *args, **kwargs):
         tk.Frame.__init__(self, root, *args, **kwargs)
         self.root = root
         self.configure_GUI()
+        self.set_country_list(countries)
         self.create_widgets()
         
 
@@ -15,20 +20,59 @@ class App(tk.Frame):
 
 
     def create_widgets(self):
-        text1 = tk.Label(self, text="Find information about a ZIP code from a particular country!")
-        text1.pack()
+        self.result_frame = tk.Frame(self, borderwidth=3, relief=tk.GROOVE)
+        self.text1 = tk.Label(self, text="Find information about a ZIP code from a particular country!")
+        self.text2 = tk.Label(self, text="Select the country: ")
+        self.text3 = tk.Label(self, text="Enter the ZIP code")
+        
+    
+        self.countryvar = tk.StringVar(self, "United States")
+        self.countrybox = ttk.Combobox(self, textvariable= self.countryvar, width = 25)
+        self.countrybox['values'] = self.supported_countries
+        self.countrybox.state(["readonly"])
 
-        entry1 = tk.Entry(self, text="Country")
-        entry2 = tk.Entry(self, text="ZIP")
+        self.ZIP_var = tk.StringVar(self, "")
+        self.ZIP_entry = tk.Entry(self, textvariable= self.ZIP_var, width = 15)
 
-        entry1.pack()
-        entry2.pack()
+        self.submit_btn = tk.Button(self, text="Find!", command=self.output_result, width=14, 
+            pady=2, foreground='dark green', activeforeground="dark green",
+             font = "Arial 12 bold", borderwidth=4)
 
+        self.result_string = tk.StringVar(self.result_frame, "")
+        self.result_label = tk.Label(self.result_frame, textvariable=self.result_string, 
+                                    font=('Arial 11'), width=40, height=8, wraplength=400)
 
+        self.text1.grid(row = 0, column = 0, columnspan=2, pady=15)
+        self.text2.grid(row = 1, column = 0)
+        self.text3.grid(row = 1, column = 1)
+        self.countrybox.grid(row = 2, column = 0, padx=10, pady=5)
+        self.ZIP_entry.grid(row = 2, column = 1, padx=10, pady=5)
+        self.submit_btn.grid(row=3, column = 0, columnspan=2)
+
+        self.result_frame.grid(row=4, column= 0, columnspan=2, rowspan=2, padx=10, pady=10)
+        self.result_label.grid(row=4, column= 0, columnspan=2, rowspan=2)
+
+    def set_country_list(self, countries):
+        self.supported_countries = [*supported_countries]
+
+    def output_result(self):
+        result = findby_country_and_zip(
+            get_country_code(self.countryvar.get()), self.ZIP_var.get())
+
+        if (len(result) > 1):
+            self.result_string.set("The code {} points to the city of {} "
+             "located in the state of {}.\n\nlatitude: {}       longitude: {}".format(
+                 self.ZIP_var.get(), result["places"][0]["place name"], 
+                 result["places"][0]["state"], result["places"][0]["latitude"], 
+                 result["places"][0]["longitude"]))
+        else:
+            self.result_string.set(result["error"])
+         
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("Find Zip Info")
-    App(root).pack(side="top", fill="both", expand=True)
+    App(root, supported_countries, borderwidth=3, relief=tk.RIDGE).pack(
+            expand=True, side="top", fill="both", padx=1, pady=1)
     root.mainloop()
 
